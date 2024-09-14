5a3f1b8ff6cdc32ba7e6fc8f8093569b683b61f6
johana.tw <johana.nunhez14@gmail.com>
2024-09-14 12:39:02 -0400

    se agrego nueva columna

--- krazystore_/src/main/java/com/krazystore/krazystore/Entity/PrecioVentaEntity.java
+++ krazystore_/src/main/java/com/krazystore/krazystore/Entity/PrecioVentaEntity.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 /**
@@ -23,4 +25,8 @@ public class PrecioVentaEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_producto")
+    private ProductoEntity producto;
 }
